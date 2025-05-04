cf673df0ebdf7872fa2a8cb05cc1a4fe26fd0572
Debraj Sarkar <debraj1127s@gmail.com>
2025-05-04 15:06:09 +0530

    Update server.js

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -30,11 +30,11 @@ app.use("/api", campaignRoutes);
 app.use("/api", authRoutes);
 
 // MongoDB connection
+require("dotenv").config();
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Cron job to update campaign statuses every minute
