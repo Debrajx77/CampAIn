6e566dc7f2c341c016538403a72f1fa336d469a4
Debraj <debraj1127s@gmail.com>
2025-05-15 14:26:13 +0530

    ui

--- Frontend/vite-project/src/Routes/CreateCampaign.jsx
+++ Frontend/vite-project/src/Routes/CreateCampaign.jsx
@@ -75,7 +75,6 @@ function CreateCampaignPage() {
     startDate: "",
     endDate: "",
     status: "Draft",
-    channels,
   });
   // Channel selection and configuration
   const [selectedChannels, setSelectedChannels] = useState([]);
