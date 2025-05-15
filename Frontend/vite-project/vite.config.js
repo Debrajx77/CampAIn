a644081463dd44566386705e1dbc41a556a6a9a1
Debraj <debraj1127s@gmail.com>
2025-05-16 02:33:36 +0530

    ui

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -5,6 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mui: ["@mui/material", "@mui/icons-material", "@emotion/react"],
+          charts: ["chart.js", "react-chartjs-2"],
+          utils: ["html2canvas", "jspdf", "moment", "dom-to-image-more"],
+          draft: ["draft-js"],
+          animation: ["framer-motion"],
+        },
+      },
+    },
     sourcemap: true, // ✅ Enable source maps for debugging
   },
 });
